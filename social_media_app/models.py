91a84c84f7dc772330a8cb7d57e4c2f8ebe08aa3
Yash <jewalkaryash@gmail.com>
2023-09-19 10:09:15 +0530

    homepage

---
+++ social_media_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+User= get_user_model()
+
+class profile(models.Model):
+    user=models.OneToOneField(User, related_name='profile',on_delete=models.CASCADE)
+    discription= models.TextField(default="")
+    image= models.ImageField()
+    birthday=models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+
+
+
+class post(models.Model):
+    user=models.ForeignKey(User, related_name='post',on_delete=models.CASCADE)
+    discription= models.TextField(default="")
+    image= models.ImageField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+
+
+
+class likepost(models.Model):
+    user_id=models.ForeignKey(User,related_name="likepost",on_delete=models.CASCADE)
+    post_id=models.ForeignKey(post,related_name="likepost",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
